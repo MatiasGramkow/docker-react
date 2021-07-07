91520bbc63b9083ba11d7ed02fa47590c451624d
MatiasGramkow <matias_gramkow@hotmail.com>
2021-07-07 14:09:08 +0200

    changed app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hej Far! 
+          Docker er for vildt! 
         </p>
         <a
           className="App-link"
